143410f1b9227584a81a18acd307fd20c5000adb
zakzakst <e1433104@gmail.com>
2023-09-12 18:02:56 +0900

    coverage

--- src/test/example.test.js
+++ src/test/example.test.js
@@ -1,5 +1,14 @@
-import { expect, test } from "vitest";
+import { expect, test, it } from "vitest";
+import multiply from "../utils/multiply";
 
 test("two plus two is four", () => {
   expect(2 + 2).toBe(4);
+});
+
+it("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
+
+test("takes two numbers as arguments and multiplies them", () => {
+  expect(multiply(2, 3)).toBe(6);
 });
---
+++ src/utils/multiply.js
@@ -0,0 +1,6 @@
+const multiply = (num1, num2) => {
+  let result = num1 * num2;
+  return result;
+};
+
+export default multiply;
